9d492847716dcfb050163faf07efcbee03c87faf
shayansalesi <shayansalesi@10.1.136.148>
2016-09-13 11:42:36 -0400

    Homework One

---
+++ Unit One/src/com/bayviewglen/dayfour/HomeworkOne.java
@@ -0,0 +1,15 @@
+package com.bayviewglen.dayfour;
+
+public class HomeworkOne {
+
+	public static void main(String[] args) {
+		
+		double num = 6.2;
+		System.out.println("The value of the number: " + num);
+		
+		double squareNum = num * num;
+		System.out.println("The square of the number is: " + squareNum);
+
+	}
+
+}
